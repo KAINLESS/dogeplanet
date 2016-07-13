f7c94b22fcab2a2aa7ba2879aedad09456361f0e
Christopher Posada <cp@bLAKjAK.home>
2016-07-13 03:54:41 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Dogeplanet - Coming Soon</title>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> <!-- 33 KB -->
+
+<!-- fotorama.css & fotorama.js. -->
+<link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet"> <!-- 3 KB -->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script> <!-- 16 KB -->
+    <!-- GA Script -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-6178016-10', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+<body style="margin: 0; overflow: hidden; background: #000; position: relative;">
+<!-- 2. Add images to <div class="fotorama"></div>. -->
+<div class="fotorama"
+     data-fit="cover"
+     data-width="100%"
+     data-height="100%"
+     data-hash="true"
+     data-loop="true"
+     data-autoplay="true"
+     data-transition="crossfade"
+     data-nav="false">
+
+    <img src="images/bush-doge.jpg" data-position="25% 35%">
+    <img src="images/dogeplanet.jpg">
+    <img src="images/smileydoge.jpg">
+    <img src="images/backpackdoge.jpg">
+    <img src="images/kitchen-doge.jpg" data-position="70% 15%">
+</div>
+
+<!-- 3. Enjoy! -->
+</body>
+</html>
